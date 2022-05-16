0791a7f68cfa1dfe2292a60c70843ea492f71936
Jaiharish-passion07 <ppjairam2@gmail.com>
2022-05-16 11:40:53 +0530

    Webapp commit changes

--- webapp/login.py
+++ webapp/login.py
@@ -3,14 +3,19 @@ from webapp.signup_page import *
 
 
 with pglet.page("Management") as page:
+    page.theme_primary_color = '#6714b5'
+    page.theme_text_color = '#091382'
+    page.theme_background_color = '#daf035'
+    page.update()
+
     class display_login():
         def __init__(self,user):
             self.task=[]
             self.user=user
-            self.tb1 = Textbox(label=self.user, required=True)
-            self.tb2 = Textbox(label='Password', password=True, required=True)
-            self.b = Button("Submit", on_click=self.button_clicked)
-            self.view = Stack(controls=[self.tb1, self.tb2, self.b])
+            self.tb1 = Textbox(label=self.user, required=True,align='center')
+            self.tb2 = Textbox(label='Password', password=True, required=True,align='center')
+            self.b = Button("Submit", on_click=self.button_clicked,primary=True)
+            self.view = Stack(padding=100,border_style='dashed',border_color='black',width='40%',border_radius=10,controls=[self.tb1, self.tb2, self.b])
 
 
         def button_clicked(self,e):
@@ -28,8 +33,10 @@ with pglet.page("Management") as page:
             if self.tb1.value!='' and self.tb2.value!='':
                 if self.user=="Faculty User-Id":
                     signup_display(label="Faculty Name")
+                    #page.disabled(True)
                 elif self.user=="Student -Id":
                     signup_display(label="Student Name")
+                    #page.disabled(True)
 
             self.view.update()
 
@@ -37,8 +44,9 @@ with pglet.page("Management") as page:
     app1 =display_login("Faculty User-Id")
     app2=display_login("Student -Id")
     # add application's root control to the page
-    page.add(Stack(horizontal=True,horizontal_align = "start",controls=[app1.view,app2.view]))
+    page.add(Stack(padding=100,horizontal=True,border_style='solid',border_color='black',horizontal_align='space-evenly',width='100%',controls=[app1.view,app2.view]))
     input()
+
 
 
 
--- webapp/signup_page.py
+++ webapp/signup_page.py
@@ -1,10 +1,14 @@
 import pglet
-from pglet import Textbox,Dropdown, dropdown,DatePicker
+from pglet import Textbox,Dropdown, dropdown,DatePicker,Stack
 from datetime import datetime
 
 
 def signup_display(label):
     with pglet.page("staff_sign") as student_signup_page:
+        student_signup_page.theme_primary_color = '#6714b5'
+        student_signup_page.theme_text_color = '#091382'
+        student_signup_page.theme_background_color = '#daf035'
+        student_signup_page.update()
         now = datetime.now()
         email_id= Textbox(label='Email-Id',suffix="@gmail.com")
         phone_no = Textbox(label='Phone Number', required=True)
@@ -26,10 +30,16 @@ def signup_display(label):
                 dropdown.Option('Senior Professor'),
                 dropdown.Option('Assistant Professor'), dropdown.Option('Technician')])
 
-            return student_signup_page.add(name,email_id,fac_id,fac_role,phone_no,address,pawd,rpwad,dept_name,dob)
+            layout1_faculty = Stack(padding=100,border_style='dashed',border_color='black',width='40%',border_radius=10,controls=[name,email_id,fac_id,fac_role,phone_no])
+            layout2_faculty = Stack(padding=100,border_style='dashed',border_color='black',width='40%',border_radius=10,controls=[address,pawd,rpwad,dept_name,dob])
+            return student_signup_page.add(Stack(padding=100,horizontal=True,border_style='solid',border_color='black',horizontal_align='space-evenly',width='100%',controls=[layout1_faculty,layout2_faculty]))
         if label=="Student Name":
             stu_id = Textbox(label='Student Id', required=True)
             name = Textbox(label='Student Name')
 
-            return student_signup_page.add(name,email_id,stu_id,phone_no,address,pawd,rpwad,dept_name,dob)
+            layout1_stu = Stack(padding=100,border_style='dashed',border_color='black',width='40%',border_radius=10,controls=[name, email_id, stu_id, phone_no])
+            layout2_stu = Stack(padding=100,border_style='dashed',border_color='black',width='40%',border_radius=10,controls=[address, pawd, rpwad, dept_name, dob])
 
+            return student_signup_page.add(Stack(padding=100,horizontal=True,border_style='solid',border_color='black',horizontal_align='space-evenly',width='100%',controls=[layout1_stu,layout2_stu]))
+
+        input()
